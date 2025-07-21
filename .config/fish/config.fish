7e112bea7afe7726226cac8890a93e555b6f6ce6
VerdantEli <miguelsicat8@gmail.com>
2025-07-22 00:04:11 +0800

    Major change to hyprlock, and new theme for sddm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,11 +1,14 @@
 if status is-interactive
 
-set fish_greeting
+function fish_greeting
+fastfetch --logo ~/Pictures/KittyNeofetch/RalseiSplat.png --logo-type kitty-direct --logo-width 25 --logo-padding-left 2 --logo-padding-top 1
+end
+
 alias ls='ls --color=auto'
 alias grep='grep --color=auto'
 
 starship init fish | source
-set -x  XDG_CURRENT_DESKTOP Hyprland
+set -x XDG_CURRENT_DESKTOP Hyprland
 set -x MICRO_TRUECOLOR 1
     # Commands to run in interactive sessions can go here
 end
