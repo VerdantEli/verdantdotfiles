996446258140d164b236e9054653b4bd819016df
VerdantEli <61437567+VerdantEli@users.noreply.github.com>
2025-07-22 16:18:31 +0800

    Fixed welcomekitty.sh

--- .config/hypr/welcomekitty.sh
+++ .config/hypr/welcomekitty.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 kitty --class terminal & 
 hyprctl dispatch movecursor 1920 1079 
+sleep 0.5
 hyprctl dispatch focuswindow "class:^terminal"
-sleep 0.2
 wtype -d 10 " "Krusie: ???????????????????????????"        "\\"        "Ralsei: VerdantEli pushed me" "
 hyprctl dispatch movecursor 960 540
