bc98f2052f3a27d73ce00ea5538ad4330f8d2900
VerdantEli <miguelsicat8@gmail.com>
2025-07-24 14:39:51 +0800

    Converted Qwerty keybinds to Colemak-DH and minor changes

--- .config/hypr/welcomekitty.sh
+++ .config/hypr/welcomekitty.sh
@@ -2,6 +2,8 @@
 kitty --class terminal & 
 hyprctl dispatch movecursor 1920 1079 
 sleep 0.5
+=======
+sleep 2
 hyprctl dispatch focuswindow "class:^terminal"
 wtype -d 10 " "Krusie: ???????????????????????????"        "\\"        "Ralsei: VerdantEli pushed me" "
 hyprctl dispatch movecursor 960 540
