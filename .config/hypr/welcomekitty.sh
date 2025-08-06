7df69fe64c7b04d3b457f956d2c1897d0f18a23d
VerdantEli <miguelsicat8@gmail.com>
2025-08-06 23:40:11 +0800

    Fixed hyprland.conf and welcomekitty.sh

--- .config/hypr/welcomekitty.sh
+++ .config/hypr/welcomekitty.sh
@@ -4,7 +4,7 @@ hyprctl dispatch movecursor 1920 1079
 sleep 2
 hyprctl dispatch focuswindow "class:^terminal"
 wtype -d 5 " "Krusie: ???????????????????????????"        "\\"        "Ralsei: VerdantEli pushed me" "
-hyprctl dispatch movecursor 960 540
+sleep 0.2
 hyprctl dispatch focuswindow "class:^io.github.TheWisker.Cavasik"
-sleep 2
 hyprctl dispatch focuswindow "class:^terminal"
+hyprctl dispatch movecursor 960 540
